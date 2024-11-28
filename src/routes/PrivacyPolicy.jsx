b0ad34b7e43a92dde83bd1ad319a5d6e0f83ab7c
hemangdigitalxplode <hemang@digitalxplode.com>
2024-11-28 15:30:47 +0530

    design changed privacy-page

--- src/routes/PrivacyPolicy.jsx
+++ src/routes/PrivacyPolicy.jsx
@@ -12,15 +12,20 @@ const PrivacyPolicy = () => {
                 </div>
             </div>
             <div className='col-lg-12 container'>
-                <h1 className='top-heading'>Privacy Policy</h1>
+
                 <p className='t-description'>
-                    We may share your personal information with third-party service providers that help us execute our training programs, but only with your consent. These providers are bound to use your information strictly to deliver services to us and must adhere to our privacy policies. We will not sell or share your information with other parties for any other reason.
+                    <li>  We may share your personal information with third-party service providers that help us execute our training programs, but only with your consent. These providers are bound to use your information strictly to deliver services to us and must adhere to our privacy policies. We will not sell or share your information with other parties for any other reason.</li> <br />
 
-                    This policy applies to all services provided through our website. By using our website, you agree to our Privacy Policy.
+                    <li>This policy applies to all services provided through our website. By using our website, you agree to our Privacy Policy.</li><br />
 
-                    We try to keep your personal information safe from unauthorized access or disclosure. We keep your information on secure computer systems with restricted access in monitored surroundings. We encrypt susceptible information to secure your data.
+                    <li>
+                        We try to keep your personal information safe from unauthorized access or disclosure. We keep your information on secure computer systems with restricted access in monitored surroundings. We encrypt susceptible information to secure your data.
+                    </li><br />
 
-                    You have the option of whether or not to share your personal information with us. If you choose not to get involved, you could end up not being able to access some features, offers, and services that rely on your participation with us. If you decide to build a relationship with us, we will continue to interact with you as part of that relationship.
+                    <li>
+                        You have the option of whether or not to share your personal information with us. If you choose not to get involved, you could end up not being able to access some features, offers, and services that rely on your participation with us. If you decide to build a relationship with us, we will continue to interact with you as part of that relationship.
+                    </li><br />
+
 
 
                 </p>
@@ -28,13 +33,17 @@ const PrivacyPolicy = () => {
             <div className='col-lg-12 container mt-4'>
 
                 <p className='t-description'>
-                    When we accumulate your personal information, we will explain why we need it and if we plan to disclose it to anybody outside the organization or those working for us. We will not share your personal information with third parties without your consent unless legally necessary.
+                    <li> When we accumulate your personal information, we will explain why we need it and if we plan to disclose it to anybody outside the organization or those working for us. We will not share your personal information with third parties without your consent unless legally necessary.</li><br />
 
-                    If you provide us with personal information, we will keep it accurate and allow you to update or change it. Your data will be confidential and used exclusively for research, marketing, and business analysis. We will not sell or rent your personal information. We may share your information with our business partners or franchisees with your permission. We will additionally share your information as required by law.
+                    <li>  If you provide us with personal information, we will keep it accurate and allow you to update or change it. Your data will be confidential and used exclusively for research, marketing, and business analysis. We will not sell or rent your personal information. We may share your information with our business partners or franchisees with your permission. We will additionally share your information as required by law.
+                    </li> <br />
 
-                    By using our website, you agree to the terms of this Privacy Policy and the use of your personal information for the reasons specified. At the time of collection, we will tell you how your information will be used. We generally use your information to reply to inquiries, process orders, and give access to certain services. You will have the option of restricting access to your information. If you fail to respond to limit access, we assume you agree to the distribution of your information as stated.
 
-                    Please remember that this Privacy Policy may change at any time without prior notification. Please email us if you have any concerns regarding our Privacy Policy or how we handle your personal information.
+
+                    <li>    By using our website, you agree to the terms of this Privacy Policy and the use of your personal information for the reasons specified. At the time of collection, we will tell you how your information will be used. We generally use your information to reply to inquiries, process orders, and give access to certain services. You will have the option of restricting access to your information. If you fail to respond to limit access, we assume you agree to the distribution of your information as stated.</li><br />
+
+                    <li>  Please remember that this Privacy Policy may change at any time without prior notification. Please email us if you have any concerns regarding our Privacy Policy or how we handle your personal information.</li><br />
+
                 </p>
             </div>
         </div>
